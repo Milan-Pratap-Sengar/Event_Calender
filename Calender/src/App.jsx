9d000109e51021de1cd74866effe73cf3206a575
Milan Pratap Sengar <milanpratap1407@gmail.com>
2025-07-20 23:27:44 +0530

    first commit

---
+++ Calender/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css'
+import CalenderApp from "./components/CalenderApp";
+
+function App(){
+  return(
+    <div className="container">
+      <CalenderApp/>
+    </div>
+  )
+}
+
+export default App;
---
+++ Calender/src/components/CalenderApp.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react"
+import "./CalenderApp.css"
+
+
+function CalenderApp(){
+
+    const days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+    const months=["January","February","March","April","May","June","July","August","September","October","November","December"]
+    const currentDate=new Date()
+    const [currentMonth, setCurrentMonth]=useState(currentDate.getMonth())
+    const [currentYear, setCurrentYear]=useState(currentDate.getFullYear())
+    const daysInMonth=new Date(currentYear, currentMonth+1 , 0).getDate();
+    const firstDayOfMonth=new Date(currentYear,currentMonth, 1).getDay();
+    const [selectedDate,setSelectedDate]=useState(currentDate)
+    const [showEventPopup, setShowEventPopup]=useState(false)
+    console.log(currentDate,currentMonth,currentYear,daysInMonth,firstDayOfMonth)
+
+    const prevMonth=()=>{
+        setCurrentMonth((prevMonth) => (prevMonth === 0 ? (11):(prevMonth-1)))
+        setCurrentYear((prevYear) => (currentMonth === 0 ? (prevYear-1):(prevYear)))
+    }
+
+    const nextMonth=()=>{
+        setCurrentMonth((prevMonth) => (prevMonth === 11 ? (0):(prevMonth+1)))
+        setCurrentYear((prevYear) => (currentMonth === 11 ? (prevYear+1):(prevYear)))
+    }
+
+    const handleDayClick= (day)=>{
+        const clickedDate=new Date(currentYear, currentMonth, day)
+        const today=new Date()
+
+        if(clickedDate >=today || isSameDay(clickedDate,today)){
+            setSelectedDate(clickedDate)
+            setShowEventPopup(true)
+
+        }
+    }
+
+    const isSameDay=(date1, date2)=>{
+        return (
+            date1.getFullYear() === date2.getFullYear() && date1.getMonth() === date2.getMonth() && date1.getDate() === date2.getDate()
+        )
+    }
+
+    return (
+        <div className="calendar-app">
+            <div className="calendar">
+                <h1 className="heading">Calendar</h1>
+                <div className="navigate-date">
+                    <h2 className="month">{months[currentMonth]},</h2>
+                    <h2 className="year">{currentYear}</h2>
+                    <div className="buttons">
+                        <i className="bx bx-chevron-left" onClick={prevMonth}></i>
+                        <i className="bx bx-chevron-right" onClick={nextMonth}></i>
+                    </div>
+                </div>
+                <div className="weekdays">
+                    {
+                        days.map((day,index)=> <span key={index}>{day}</span>)
+                    }
+                </div>
+                <div className="days">
+                    {
+                        [...Array(firstDayOfMonth).keys()].map((_,index)=> <span key={`empty-${index} `}/>)
+                    }
+                    {
+                        [...Array(daysInMonth).keys()].map((day)=> <span key={day+1} className={day+1 === currentDate.getDate() && currentMonth===currentDate.getMonth() && currentYear===currentDate.getFullYear() ? "current-day" : "" } onClick={()=>handleDayClick(day+1)}>{day+1}</span>)
+                    }
+                </div>
+            </div>
+            <div className="events">
+                {
+                    showEventPopup && <div className="event-popup">
+                                        <div className="time-input">
+                                            <div className="event-popup-time">Time</div>
+                                            <input type="number" name="hours" min={0} max={24} className="hours bg-white"/>
+                                            <input type="number" name="minutes" min={0} max={60} className="minutes bg-white"/>
+                                        </div>
+                                        <textarea placeholder="Enter Event Text (Maximum 60 Characters)" className="bg-white"></textarea>
+                                        <button className="event-popup-btn bg-white">Add Event</button>
+                                        <button className="close-event-popup" onClick={()=>setShowEventPopup(false)}>
+                                            <i className="bx bx-x bg-white"></i>
+                                        </button>
+                                    </div>
+                }
+                <div className="event">
+                    <div className="event-date-wrapper">
+                        <div className="event-date">May 15, 2024</div>
+                        <div className="event-time">10:00</div>
+                    </div>
+                    <div className="event-text">Meeting with John</div>
+                    <div className="event-buttons">
+                        <i className="bx bxs-edit-alt"></i>
+                        <i class='bx  bx-message-plus'  ></i> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CalenderApp;
