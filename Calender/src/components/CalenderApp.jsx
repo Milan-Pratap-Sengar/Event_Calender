300b7e5b00596668356dc201f0899eb217a25571
Milan Pratap Sengar <milanpratap1407@gmail.com>
2025-07-21 15:44:39 +0530

    update

--- Calender/src/components/CalenderApp.jsx
+++ Calender/src/components/CalenderApp.jsx
@@ -90,6 +90,12 @@ function CalenderApp(){
         setEvents(updateEvents)
     }
 
+    const handleTimeChange= (e)=>{
+        const {name,value}=e.target;
+        setEventTime((prevTime)=> ({...prevTime, [name]:value.padStart(2,'0')}))
+    }
+
+
     return (
         <div className="calendar-app">
             <div className="calendar">
@@ -121,8 +127,8 @@ function CalenderApp(){
                     showEventPopup && <div className="event-popup">
                                         <div className="time-input">
                                             <div className="event-popup-time">Time</div>
-                                            <input type="number" name="hours" min={0} max={24} className="hours bg-white" value={eventTime.hours} onChange={(e)=>setEventTime({...eventTime, hours:e.target.value})}/>
-                                            <input type="number" name="minutes" min={0} max={59} className="minutes bg-white" value={eventTime.minutes} onChange={(e)=>setEventTime({...eventTime, minutes:e.target.value})}/>
+                                            <input type="number" name="hours" min={0} max={23} className="hours bg-white" value={eventTime.hours} onChange={handleTimeChange}/>
+                                            <input type="number" name="minutes" min={0} max={59} className="minutes bg-white" value={eventTime.minutes} onChange={handleTimeChange}/>
                                         </div>
                                         <textarea placeholder="Enter Event Text (Maximum 60 Characters)" className="bg-white" value={eventText} onChange={(e)=>{
                                             if(e.target.value.length <= 60){
